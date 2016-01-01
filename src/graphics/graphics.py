e4f7932e5c32329405cecc803496000f6e888bd3
Ben Weedon <ben.weedon@outlook.com>
2016-01-01 01:07:32 -0500

    Some basic PyGame implementation.

--- src/graphics/graphics.py
+++ src/graphics/graphics.py
@@ -1,5 +1,5 @@
 class Graphics(object):
-    def init_window(self, width, height):
+    def init_window(self, width, height, caption):
         raise NotImplementedError()
-    def draw_tile(self, character, foreground_color, background_color):
+    def draw_tile(self, character, fg_color, bg_color):
         raise NotImplementedError()
--- src/graphics/pygame_graphics.py
+++ src/graphics/pygame_graphics.py
@@ -1,1 +1,15 @@
-class Pygame
+import pygame
+import graphics
+
+class PyGameGraphics(graphics.Graphics):
+    def __init__(self):
+        pygame.init()
+    
+    def init_window(self, width, height, caption):
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(caption)
+    
+    def draw_tile(self, character, fg_color, bg_color):
+        image = pygame.image.load('..\res\font.png').convert()
+        screen.blit(image)
+        pygame.display.update()
