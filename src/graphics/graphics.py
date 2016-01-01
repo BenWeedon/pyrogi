c3a31302321dae21d05ff72d55fe11c65aaf548e
Ben Weedon <ben.weedon@outlook.com>
2015-12-31 23:51:11 -0500

    Graphics interface

---
+++ src/graphics/graphics.py
@@ -0,0 +1,5 @@
+class Graphics(object):
+	def init_window(self, width, height):
+		raise NotImplementedError()
+	def draw_tile(self, character, foreground_color, background_color):
+		raise NotImplementedError()
