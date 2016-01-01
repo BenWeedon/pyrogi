57f3df929c5338c01a3e8524a973f49742aea01a
Ben Weedon <ben.weedon@outlook.com>
2016-01-01 02:07:52 -0500

    Some more work running from VSCode and also it kinda displays, but with errors.

--- src/graphics/pygame_graphics.py
+++ src/graphics/pygame_graphics.py
@@ -6,10 +6,10 @@ class PyGameGraphics(graphics.Graphics):
         pygame.init()
     
     def init_window(self, width, height, caption):
-        screen = pygame.display.set_mode((width, height))
+        self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption(caption)
     
     def draw_tile(self, character, fg_color, bg_color):
-        image = pygame.image.load('..\res\font.png').convert()
-        screen.blit(image)
+        image = pygame.image.load('res/font.png').convert()
+        self.screen.blit(image)
         pygame.display.update()
---
+++ src/main.py
@@ -0,0 +1,5 @@
+import graphics.pygame_graphics
+
+graphics = graphics.pygame_graphics.PyGameGraphics()
+graphics.init_window(100, 100, 'test')
+graphics.draw_tile(None, None, None)
