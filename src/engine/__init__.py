535650c46d3564fdec6f057f1cbacf2e8c9dfe8f
Ben Weedon <ben.weedon@outlook.com>
2016-01-01 18:42:40 -0500

    Added basic font config stuff, and used it to draw '@'. Also, we're using the Brogue font for now.

---
+++ src/engine/__init__.py
@@ -0,0 +1,4 @@
+import os.path
+
+FONT_PATH = os.path.join('res', 'fonts')
+FONT_CONFIG_EXTENSION = '.font.json'
--- src/engine/graphics/pygame_graphics.py
+++ src/engine/graphics/pygame_graphics.py
@@ -1,4 +1,7 @@
+import os.path
+import json
 import pygame
+from engine import FONT_PATH, FONT_CONFIG_EXTENSION
 from engine.graphics import Graphics
 from engine.util.vector import Vec2
 
@@ -8,12 +11,32 @@ class PyGameGraphics(Graphics):
         pygame.display.set_caption(caption)
     
     def draw_tile(self, position, character, fg_color, bg_color):
-        font_image = pygame.image.load('res/font.png').convert()
-        tile_image = self._get_tile_image(font_image, Vec2(0, 0))
+        font_image, font_config = self._load_font('brogue.png')
+        tile_image = self._get_tile_image(font_image, font_config, Vec2(0, 4))
         self.screen.blit(tile_image, (0, 0))
         pygame.display.update()
     
-    def _get_tile_image(self, image, index):
-        tile_image = pygame.Surface((10, 10)).convert()
-        tile_image.blit(image, (0, 0), (index.x*10, index.y*10, (index.x+1)*10, (index.y+1)*10))
+    def _load_font(self, filename):
+        font_image = pygame.image.load(os.path.join(FONT_PATH, filename)).convert()
+        config_filename = os.path.splitext(filename)[0] + FONT_CONFIG_EXTENSION
+        font_config = self._load_font_json(os.path.join(FONT_PATH, config_filename))
+        
+        return font_image, font_config
+
+    def _load_font_json(self, filename):
+        with open(filename) as f:
+            data = json.load(f)
+            font_config = FontConfig(
+                Vec2(data['tileWidth'], data['tileHeight'])
+            )
+            return font_config
+    
+    def _get_tile_image(self, image, config, index):
+        dim = config.tile_dimensions
+        tile_image = pygame.Surface((dim.x, dim.y)).convert()
+        tile_image.blit(image, (0, 0), (index.x*dim.x, index.y*dim.y, (index.x+1)*dim.x, (index.y+1)*dim.y))
         return tile_image
+
+class FontConfig(object):
+    def __init__(self, tile_dimensions):
+        self.tile_dimensions = tile_dimensions
