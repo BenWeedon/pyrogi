a8efb9ec66f6e1dbf92474d353cbfd4833825224
Ben Weedon <ben.weedon@outlook.com>
2016-01-01 00:37:02 -0500

    Tabs to spaces.

--- src/graphics/graphics.py
+++ src/graphics/graphics.py
@@ -1,5 +1,5 @@
 class Graphics(object):
-	def init_window(self, width, height):
-		raise NotImplementedError()
-	def draw_tile(self, character, foreground_color, background_color):
-		raise NotImplementedError()
+    def init_window(self, width, height):
+        raise NotImplementedError()
+    def draw_tile(self, character, foreground_color, background_color):
+        raise NotImplementedError()
