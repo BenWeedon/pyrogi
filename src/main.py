92f70d95a6f31657748b135526bb5e83c817ae92
Ben Weedon <ben.weedon@outlook.com>
2016-01-03 17:49:51 -0500

    Changed tile dimensions to be more accurate.

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from engine.backend import Game
 from engine.util.vector import Vec2
 from test_game import TestScreen
 
-PyGameBackend(Game(TestScreen()), Vec2(20, 20), Vec2(30, 30), '').run()
+PyGameBackend(Game(TestScreen()), Vec2(20, 20), Vec2(19, 33), '').run()
