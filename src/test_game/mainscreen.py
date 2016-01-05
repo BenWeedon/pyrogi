739dea48e7c828a3cd3bb172432bcff7d309c98e
Ben Weedon <ben.weedon@outlook.com>
2016-01-04 22:48:06 -0500

    Removed the extra buttons I was apparently drawing.

--- src/test_game/mainscreen.py
+++ src/test_game/mainscreen.py
@@ -5,5 +5,4 @@ from engine.util.vector import Vec2
 class MainScreen(Screen):
     def __init__(self):
         super(MainScreen, self).__init__()
-        for i in xrange(30):
-            self.add_child(Button(self, Vec2(3, 2), Vec2(10, 2), 'test[omega]button[ram]OMG~~'))
+        self.add_child(Button(self, Vec2(3, 2), Vec2(10, 2), 'test[omega]button[ram]OMG~~'))
