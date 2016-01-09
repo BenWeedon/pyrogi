858b5d3da7263d3fc9448da1d82589265706d415
Ben Weedon <ben.weedon@outlook.com>
2016-01-08 22:39:06 -0500

    Added tests for parse_text_into_characters.

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -11,39 +11,47 @@ def parse_text_into_characters(text):
     characters = []
     is_escaping = False
     group = None
+    def append(ch):
+        """append the next character to the right place"""
+        if group is None:
+            characters.append(ch)
+        else:
+            group.append(ch)
+    
     for ch in text:
         if ch == '\\':
             if is_escaping:
-                characters.append(ch)
+                append(ch)
                 is_escaping = False
             else:
                 is_escaping = True
         elif ch == '[':
             if is_escaping:
-                characters.append(ch)
+                append(ch)
                 is_escaping = False
             else:
                 if group is not None:
                     raise ValueError('You cannot start a character group within another group.')
-                group = ''
+                group = []
             is_escaping = False
         elif ch == ']':
             if is_escaping:
-                characters.append(ch)
+                append(ch)
             else:
                 if group is None:
                     raise ValueError('You cannot end a character group that you have not started.')
-                characters.append(group)
+                if len(group) == 0:
+                    raise ValueError('You cannot have an empty character group.')
+                characters.append(''.join(group))
                 group = None
             is_escaping = False
         else:
             if is_escaping:
-                raise ValueError('Invalid escape character \'' + ch + '\'.')
-            if group is None:
-                characters.append(ch)
-            else:
-                group += ch
+                raise ValueError("Invalid escape character '" + ch + "'.")
+            append(ch)
             is_escaping = False
+    if is_escaping:
+        raise ValueError("The '\\' at the end of the string isn't escaping anything.")
     if group is not None:
         raise ValueError('You started a character group but did not finish it.')
     return characters
