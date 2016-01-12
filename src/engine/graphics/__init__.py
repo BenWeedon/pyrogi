6c5ad90209c46a966a3a5d97870a1c87d844596d
Ben Weedon <ben.weedon@outlook.com>
2016-01-11 23:20:16 -0500

    Wrote and tested linear gradient code.

--- src/engine/graphics/__init__.py
+++ src/engine/graphics/__init__.py
@@ -23,6 +23,11 @@ class Tile(object):
 
 class Color(object):
     def __init__(self, r, g, b, a=255):
+        for val in [r, g, b, a]:
+            if isinstance(val, float):
+                raise ValueError("A Color object cannot contain the float value '" + str(val) + "'.")
+            if val < 0 or val > 255:
+                raise ValueError('The parameters to a Color object must be in the range [0, 255].')
         self.r = r
         self.g = g
         self.b = b
@@ -40,7 +45,9 @@ class Color(object):
 
 class Paint(object):
     def get_tile_color(self, absolute_position, relative_position):
-        """absolute_position is absolute in the window, relative_position is relative_position to the Drawable being colored"""
+        """absolute_position is absolute in the window, relative_position is relative_position to the Drawable being colored
+        The Paint may only use one of them depending on whether it draws absolutely or relatively
+        """
         raise NotImplementedError()
     def tick(self, millis):
         pass
@@ -49,6 +56,43 @@ class SolidPaint(Paint):
         self.color = color
     def get_tile_color(self, absolute_position, relative_position):
         return self.color
+class GradientPaint(Paint):
+    pass
+class LinearGradientPaint(GradientPaint):
+    def __init__(self, color1, position1, color2, position2, is_cyclical=True, is_relative=True):
+        self.color1 = color1
+        self.position1 = position1
+        self.color2 = color2
+        self.position2 = position2
+        self.is_cyclical = is_cyclical
+        self.is_relative = is_relative
+        self.direction_vector = (self.position2 - self.position1).normalized()
+    def get_tile_color(self, relative_position, absolute_position):
+        position = relative_position if self.is_relative else absolute_position
+        projection = position.dot(self.direction_vector) # project the position onto the linear direction vector
+        p1_projection = self.position1.dot(self.direction_vector)
+        p2_projection = self.position2.dot(self.direction_vector)
+        percent = self._get_percent_along_projection(projection, p1_projection, p2_projection)
+        return Color(
+            int(round(self.color1.r + percent * (self.color2.r - self.color1.r))),
+            int(round(self.color1.g + percent * (self.color2.g - self.color1.g))),
+            int(round(self.color1.b + percent * (self.color2.b - self.color1.b))),
+            int(round(self.color1.a + percent * (self.color2.a - self.color1.a))),
+        )
+    def _get_percent_along_projection(self, projection, p1_projection, p2_projection):
+        diff = max(p1_projection, p2_projection) - min(p1_projection, p2_projection)
+        raw_percent = (projection-min(p1_projection, p2_projection)) / diff
+        if self.is_cyclical:
+            must_inverse = int(raw_percent) % 2 == 1
+            percent_before_inverse = raw_percent % 1 if raw_percent >= 0 else 1-(raw_percent%1)
+            return 1-percent_before_inverse if must_inverse else percent_before_inverse
+        else:
+            if raw_percent < 0:
+                return 0
+            elif raw_percent > 1:
+                return 1
+            else:
+                return raw_percent
 
 
 class Drawable(object):
--- src/engine/graphics/test_graphics.py
+++ src/engine/graphics/test_graphics.py
@@ -1,5 +1,6 @@
 import unittest
-from engine.graphics import Color
+from engine.graphics import Color, LinearGradientPaint
+from engine.util.vector import Vec2
 
 class TestColor(unittest.TestCase):
     def test_to_tuple(self):
@@ -10,3 +11,86 @@ class TestColor(unittest.TestCase):
     def test_equals(self):
         self.assertEqual(Color(1, 2, 3, 4), Color(1, 2, 3, 4))
         self.assertEqual(Color(1, 2, 3), Color(1, 2, 3, 255))
+    
+    def test_initialization_errors(self):
+        self.assertRaisesRegexp(ValueError, "A Color object cannot contain the float value '1.1'.", Color, 1.1, 2, 3, 4)
+        self.assertRaisesRegexp(ValueError, "A Color object cannot contain the float value '2.02'.", Color, 1, 2.02, 3, 4)
+        self.assertRaisesRegexp(ValueError, "A Color object cannot contain the float value '3.7'.", Color, 1, 2, 3.7, 4)
+        self.assertRaisesRegexp(ValueError, "A Color object cannot contain the float value '4.0'.", Color, 1, 2, 3, 4.0)
+        
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, -1, 2, 3, 4)
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, 256, 2, 3, 4)
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, 1, -38, 3, 4)
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, 1, 300, 3, 4)
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, 1, 2, -3, 4)
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, 1, 2, 256, 4)
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, 1, 2, 3, -1)
+        self.assertRaisesRegexp(ValueError, 'The parameters to a Color object must be in the range \\[0, 255\\].', Color, 1, 2, 3, 500)
+
+
+class TestPaints(unittest.TestCase):
+    def test_linear_gradient(self):
+        self._run_linear_gradient_test(
+            Color(0, 0, 0, 0), Vec2(0, 0),
+            Color(255, 255, 255, 255), Vec2(10, 0),
+            Vec2(5, 0), Vec2(5, 0),
+            Color(128, 128, 128, 128), Color(128, 128, 128, 128), Color(128, 128, 128, 128), Color(128, 128, 128, 128)
+        )
+        self._run_linear_gradient_test(
+            Color(0, 0, 0, 0), Vec2(0, 0),
+            Color(255, 255, 255, 255), Vec2(10, 0),
+            Vec2(-2, 0), Vec2(-2, 0),
+            Color(51, 51, 51, 51), Color(0, 0, 0, 0), Color(51, 51, 51, 51), Color(0, 0, 0, 0)
+        )
+        self._run_linear_gradient_test(
+            Color(0, 0, 0, 0), Vec2(0, 0),
+            Color(255, 255, 255, 255), Vec2(10, 0),
+            Vec2(8, 0), Vec2(8, 0),
+            Color(204, 204, 204, 204), Color(204, 204, 204, 204), Color(204, 204, 204, 204), Color(204, 204, 204, 204)
+        )
+        
+        self._run_linear_gradient_test(
+            Color(0, 0, 0, 0), Vec2(1, 6),
+            Color(255, 255, 255, 255), Vec2(3, 17.2),
+            Vec2(2, 15), Vec2(0.5, 5),
+            Color(203, 203, 203, 203), Color(203, 203, 203, 203), Color(24, 24, 24, 24), Color(0, 0, 0, 0)
+        )
+        self._run_linear_gradient_test(
+            Color(0, 0, 0, 0), Vec2(1, 6),
+            Color(255, 255, 255, 255), Vec2(3, 17.2),
+            Vec2(4, 23), Vec2(-2, -7),
+            Color(123, 123, 123, 123), Color(255, 255, 255, 255), Color(211, 211, 211, 211), Color(0, 0, 0, 0)
+        )
+        self._run_linear_gradient_test(
+            Color(0, 0, 0, 0), Vec2(1, 6),
+            Color(255, 255, 255, 255), Vec2(3, 17.2),
+            Vec2(6, 30), Vec2(-4.5, -100),
+            Color(39, 39, 39, 39), Color(255, 255, 255, 255), Color(190, 190, 190, 190), Color(0, 0, 0, 0)
+        )
+        self._run_linear_gradient_test(
+            Color(0, 0, 0, 0), Vec2(1.2, 12.9),
+            Color(255, 255, 255, 255), Vec2(30.8, 3.05),
+            Vec2(162.7, -40.7), Vec2(-201.5, 195.2),
+            Color(139, 139, 139, 139), Color(255, 255, 255, 255), Color(3, 3, 3, 3), Color(0, 0, 0, 0)
+        )
+    
+    def _run_linear_gradient_test(
+            self, color1, position1, color2, position2,
+            relative_position, absolute_position,
+            t_t_expected_color, f_t_expected_color, t_f_expected_color, f_f_expected_color):
+        for pair in [(t_t_expected_color, (True, True)), (f_t_expected_color, (False, True)), (t_f_expected_color, (True, False)), (f_f_expected_color, (False, False))]:
+            expected_color = pair[0]
+            is_cyclical = pair[1][0]
+            is_relative = pair[1][1]
+            # test positions/colors in given order
+            gradient = LinearGradientPaint(color1, position1, color2, position2, is_cyclical, is_relative)
+            self._assertColorEqual(gradient.get_tile_color(relative_position, absolute_position), expected_color)
+            # test positions/colors in reverse order
+            gradient = LinearGradientPaint(color2, position2, color1, position1, is_cyclical, is_relative)
+            self._assertColorEqual(gradient.get_tile_color(relative_position, absolute_position), expected_color)
+    
+    def _assertColorEqual(self, color1, color2):
+        self.assertAlmostEqual(color1.r, color2.r)
+        self.assertAlmostEqual(color1.g, color2.g)
+        self.assertAlmostEqual(color1.b, color2.b)
+        self.assertAlmostEqual(color1.a, color2.a)
